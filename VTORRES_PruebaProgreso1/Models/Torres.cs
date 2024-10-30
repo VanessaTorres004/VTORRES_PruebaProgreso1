884bddff841ea30eba7e304fcdda9942eb6e7a22
VanessaTorres004 <vanessa.torres.chacon@udla.edu.ec>
2024-10-30 08:50:04 -0500

    Validations

--- VTORRES_PruebaProgreso1/Models/Torres.cs
+++ VTORRES_PruebaProgreso1/Models/Torres.cs
@@ -1,12 +1,20 @@
-﻿namespace VTORRES_PruebaProgreso1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VTORRES_PruebaProgreso1.Models
 {
     public class Torres
     {
+        [Key]
         public int Cedula { get; set; }
+        [Required]
+        [StringLength(10)]
         decimal PrecioApagar { get; set; }
-        string Nombre { get; set; }
+        [Range(100, 1000, ErrorMessage = "El precio no pueda bajar de los 100")]
 
+        string Nombre { get; set; }
+        [Required(ErrorMessage = "El nombre es necesario")]
         bool esChino { get; set; } = true;
+        [Display(Name = "dia de compra")]
         DateTime DateTime { get; set; }
         
        
