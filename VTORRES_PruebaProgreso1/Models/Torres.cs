e83ae82f05acff9ff74e26932e449457509bd7ac
VanessaTorres004 <vanessa.torres.chacon@udla.edu.ec>
2024-10-30 08:37:11 -0500

    primer modelo

---
+++ VTORRES_PruebaProgreso1/Models/Torres.cs
@@ -0,0 +1,15 @@
+﻿namespace VTORRES_PruebaProgreso1.Models
+{
+    public class Torres
+    {
+        public int Cedula { get; set; }
+        decimal PrecioApagar { get; set; }
+        string Nombre { get; set; }
+
+        bool esChino = true;
+        
+       
+        
+       
+    }
+}
