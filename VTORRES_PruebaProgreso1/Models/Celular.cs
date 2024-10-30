6fe3a45ba464161c2781e20343ce3235066f6219
VanessaTorres004 <vanessa.torres.chacon@udla.edu.ec>
2024-10-30 09:00:05 -0500

    Actalizacion modelos

---
+++ VTORRES_PruebaProgreso1/Models/Celular.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VTORRES_PruebaProgreso1.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Modelo { get; set; }
+        [MaxLength (200)]
+        [Required(ErrorMessage = "necesitas el modelo")]
+        public int year { get; set;}
+        [Required(ErrorMessage = "cual es el año?")]
+ 
+
+        [Range(100, 1000, ErrorMessage = "El precio no pueda bajar de los 100")] 
+        public decimal Precio { get; set; }
+       
+    }
+}
--- VTORRES_PruebaProgreso1/Models/Torres.cs
+++ VTORRES_PruebaProgreso1/Models/Torres.cs
@@ -8,12 +8,12 @@ namespace VTORRES_PruebaProgreso1.Models
         public int Cedula { get; set; }
         [Required]
         [StringLength(10)]
-        decimal PrecioApagar { get; set; }
+        public decimal PrecioApagar { get; set; }
         [Range(100, 1000, ErrorMessage = "El precio no pueda bajar de los 100")]
 
-        string Nombre { get; set; }
+        public string Nombre { get; set; }
         [Required(ErrorMessage = "El nombre es necesario")]
-        bool esChino { get; set; } = true;
+       public bool esChino { get; set; } = true;
         [Display(Name = "dia de compra")]
         public DateTime Diadecompra { get; set; } = DateTime.Now;
         
